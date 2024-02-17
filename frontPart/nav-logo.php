130e2a0832b0fc6e732bbe00e55b8f5dec00df17
andrideveloper26 <160312202+andrideveloper26@users.noreply.github.com>
2024-02-17 15:37:49 +0700

    Add files via upload

---
+++ frontPart/nav-logo.php
@@ -0,0 +1,12 @@
+    <!-- brand-->
+    
+    <span class="navbar bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="image/rpl.jpg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+          KASIRKU
+        </a>
+      </div>
+    </span>
+    <!--end brand-->
+    
---
+++ frontPart/navbar.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" type="text/css" media="all" />
+    <title>test</title>
+    
+  </head>
+  <body>
+    <div class="container  mt-3 shadow p-3 mb-3 bg-body-tertiary rounded border">
+     <nav class="d-flex justify-content-between">
+       <div class="nav-icon">
+           <a class=" btn btn-primary" href="index.php">HOME</a>
+         </div>
+       <div class="nav-icon">
+         <a class=" btn btn-warning" href="../produk.php">PRODUK</a>
+       </div>
+       <div class="nav-icon">
+         <a class="btn btn-primary" href="#">KATEGORI</a>
+       </div>
+       <div class="nav-icon">
+         <a class="btn btn-secondary" href="../transaksi/transaksi.php">TRANSAKSI</a>
+       </div>
+       <div class="nav-icon">
+         <a class=" btn btn-dark" href="#">PEGAWAI</a>
+       </div>
+     </nav>
+    </div>
+      
+      
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
