2f3bbcde6f3bd2be622abbe32a4b2f3657171443
andrideveloper26 <160312202+andrideveloper26@users.noreply.github.com>
2024-02-21 09:37:25 +0700

    Create sql.php

---
+++ sql.php
@@ -0,0 +1,4 @@
+$sql = "SELECT  penjualan.penjualanID , penjualan.tanggalPenjualan, penjualan.totalHarga, pelanggan.namaPelanggan, users.idUser FROM penjualan 
+                  INNER JOIN pelanggan ON pelanggan.pelangganID = penjualan.pelangganID
+                  INNER JOIN users ON users.idUser = penjualan.penjualanID
+                  ";
